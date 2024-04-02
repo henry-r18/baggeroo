8c995db73d288cbf38a98d5e96ec343972697feb
Henry Rosen <herosen@g.ucla.edu>
2024-04-02 00:57:14 -0700

    Calculate directory sizes and add ability to remove files

--- src/store.js
+++ src/store.js
@@ -1,41 +1,42 @@
 import { Store } from "@tauri-apps/plugin-store";
-import { stat } from "@tauri-apps/plugin-fs";
+import { stat, readDir } from "@tauri-apps/plugin-fs";
+import { path as Path } from "@tauri-apps/api";
 
 const selectedFilesStore = new Store("./selectedFiles.json");
+
+// TODO: make recursive
+async function calculateDirectorySize(path) {
+  let sum = 0;
+  let entries = await readDir(path);
+  for (const entry of entries) {
+    let entryPath = await Path.join(path, entry.name);
+    let fileInfo = await stat(entryPath);
+    sum += fileInfo.size;
+  }
+  return sum;
+}
 
 async function getMetadata(paths) {
   let metadata = paths.map(async (path) => {
     let fileInfo = await stat(path); // stat() returns FileInfo object
+    if (fileInfo.isDirectory) {
+      fileInfo.size = await calculateDirectorySize(path);
+    }
     return { path, ...fileInfo }; // construct metadata object
   });
   return await Promise.all(metadata);
 }
 
-async function appendSelectedFiles(newSelectedFiles) {
-  let currentSelectedFiles = await selectedFilesStore.get("selectedFiles");
-  if (currentSelectedFiles) {
-    newSelectedFiles = [...currentSelectedFiles, ...newSelectedFiles];
-  }
-  selectedFilesStore.set("selectedFiles", newSelectedFiles);
-}
-
-// FIX: troubleshoot getting currentSelectedFiles
-async function filterAlreadySelected(paths) {
-  let currentSelectedFiles = await selectedFilesStore.get("selectedFiles");
-  return paths.filter((path) => {
-    let isAlreadySelected = currentSelectedFiles.some(
-      (file) => file.path == path
-    );
-    return !isAlreadySelected;
-  });
-}
-
 async function handleNewFiles(paths) {
   try {
-    let filteredPaths = await filterAlreadySelected(paths);
+    let selectedFiles = await selectedFilesStore.get("selectedFiles");
+    let filteredPaths = paths.filter( path => 
+      !selectedFiles.some( 
+        selectedFile => selectedFile.path === path
+    )); // filter out paths already in Bag
     if (filteredPaths.length) {
-      let newFilesMetadata = await getMetadata(filteredPaths);
-      appendSelectedFiles(newFilesMetadata);
+      selectedFiles = [...selectedFiles, ...await getMetadata(filteredPaths)];
+      selectedFilesStore.set("selectedFiles", selectedFiles);
     } else {
       throw Error("Path(s) already in Bag.");
     }
@@ -44,4 +45,17 @@ async function handleNewFiles(paths) {
   }
 }
 
-export { selectedFilesStore, handleNewFiles };
+// FIX: removing all files when called
+async function removeFile(path) {
+  try {
+    let selectedFiles = await selectedFilesStore.get("selectedFiles");
+    selectedFiles = selectedFiles.filter(selectedFile => !selectedFile.path === path);
+    selectedFilesStore.set("selectedFiles", selectedFiles);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+selectedFilesStore.set("selectedFiles", [])
+
+export { selectedFilesStore, handleNewFiles, removeFile };
